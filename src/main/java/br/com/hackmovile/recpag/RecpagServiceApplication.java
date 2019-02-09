78791e6bcd93aaf5b3117dee91843cc6ca21c06b
Cleibson Gomes <cleibson.gomes.silva@gmail.com>
2019-02-09 17:33:54 -0300

    Init 2
    Atualização basica

---
+++ src/main/java/br/com/hackmovile/recpag/RecpagServiceApplication.java
@@ -0,0 +1,14 @@
+package br.com.hackmovile.recpag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecpagServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecpagServiceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/br/com/hackmovile/recpag/RecpagServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.hackmovile.recpag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecpagServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
